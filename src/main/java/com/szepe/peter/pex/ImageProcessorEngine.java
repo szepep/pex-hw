fb9f5e64595eebc34f0e0ad5df80dc1266352d83
Peter Szepe <pszepe@chemaxon.com>
2019-07-01 20:09:17 +0200

    Collecting some stats:
    327556238200    47%	Download image
    319148070900    45%	Count colors
    49238559200	     7%	Select top K

--- src/main/java/com/szepe/peter/pex/ImageProcessorEngine.java
+++ src/main/java/com/szepe/peter/pex/ImageProcessorEngine.java
@@ -1,18 +1,27 @@
 package com.szepe.peter.pex;
 
-import com.google.common.collect.Ordering;
+import com.google.common.collect.Comparators;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.URL;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+import java.util.logging.Logger;
 
 public class ImageProcessorEngine {
 
-    private final long[][][] colors = new long[256][256][256];
+    private final static Logger logger = Logger.getLogger(ImageProcessorEngine.class.getName());
+    private final static StopWatch clock = new StopWatch();
+
+    public static void printStats() {
+        logger.info(clock.prettyPrint());
+    }
+
     private final String imageUrl;
     private final int k;
 
@@ -21,32 +30,46 @@ public class ImageProcessorEngine {
         this.k = k;
     }
 
-    private BufferedImage readImage() throws IOException {
-        URL url = new URL(imageUrl);
-        return ImageIO.read(url);
+    private Optional<BufferedImage> readImage(){
+        try {
+            URL url = new URL(imageUrl);
+            BufferedImage read = ImageIO.read(url);
+            return Optional.of(read);
+        } catch (IOException e) {
+            logger.warning("Unable to read image " + imageUrl);
+            return Optional.empty();
+        }
     }
 
-    public List<Pair<Color, Long>> process() throws IOException {
-        processImage();
+    public List<Pair<Color, Integer>> process() throws IOException {
+        Optional<BufferedImage> image = clock.start("Download image", () -> readImage());
+        if (image.isPresent()) {
+            int[][][] colors = clock.start("Count colors", () -> processImage(image.get()));
+            List<Pair<Color, Integer>> pairs = clock.start("Select top K", () -> getTopK(colors));
+            return pairs;
+        } else {
+            return Collections.emptyList();
+        }
+    }
 
-        List<Pair<Color, Long>> list = new ArrayList<>();
+    private List<Pair<Color, Integer>> getTopK(int[][][] colors) {
+        List<Pair<Color, Integer>> list = new ArrayList<>();
         for (int r = 0; r < 256; ++r) {
             for (int g = 0; g < 256; ++g) {
                 for (int b = 0; b < 256; ++b) {
-                    long count = colors[r][g][b];
+                    int count = colors[r][g][b];
                     if (count > 0) {
                         list.add(new Pair<>(new Color(r, g, b), count));
                     }
                 }
             }
         }
-        List<Pair<Color, Long>> pairs = Ordering.natural().greatestOf(list, k);
 
-        return pairs;
+        return list.stream().collect(Comparators.greatest(k, Pair::compareTo));
     }
 
-    private void processImage() throws IOException {
-        BufferedImage image = readImage();
+    private int[][][] processImage(BufferedImage image) {
+        int[][][] colors = new int[256][256][256];
         for (int x = 0; x < image.getWidth(); ++x) {
             for (int y = 0; y < image.getHeight(); ++y) {
                 int rgb = image.getRGB(x, y);
@@ -57,6 +80,7 @@ public class ImageProcessorEngine {
                 colors[r][g][b]++;
             }
         }
+        return colors;
     }
 
 
--- src/main/java/com/szepe/peter/pex/Main.java
+++ src/main/java/com/szepe/peter/pex/Main.java
@@ -6,7 +6,6 @@ import com.szepe.peter.pex.spi.InputReader;
 import java.awt.*;
 import java.io.IOException;
 import java.util.List;
-import java.util.Map;
 
 public class Main {
 
@@ -15,14 +14,16 @@ public class Main {
     }
 
     void process() throws InputReader.InputReaderException {
-        InputReader reader = new FileReader("./test_data/short.txt");
+        InputReader reader = new FileReader("./test_data/input.txt");
         reader.get().forEach(url -> {
             try {
-                List<ImageProcessorEngine.Pair<Color, Long>> process = new ImageProcessorEngine(url, 3).process();
+                List<ImageProcessorEngine.Pair<Color, Integer>> process = new ImageProcessorEngine(url, 3).process();
                 System.out.println(process);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         });
+
+        ImageProcessorEngine.printStats();
     }
 }
